c7fbbb0de31ff23c3a6311a3aad78f1b08189ada
horacio88mondaca <horaciomondaca88@gmail.com>
2020-11-11 16:25:34 +0200

    probando

--- backend/build/controllers/tipoobra.controller.js
+++ backend/build/controllers/tipoobra.controller.js
@@ -9,6 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.tipoobraController = void 0;
 const database_1 = require("../database");
 class tipoobraController {
     //Listar todos los tipos de obras
