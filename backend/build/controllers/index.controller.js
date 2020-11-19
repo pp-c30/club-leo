323496a1b970c16a691503b009f9eb736f025f11
CamiRomero28 <romeroc@cet30.edu.ar>
2020-11-19 16:26:50 -0300

    subo cambios build

--- backend/build/controllers/index.controller.js
+++ backend/build/controllers/index.controller.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.mensaje_bienvenida = void 0;
 function mensaje_bienvenida(Req, Res) {
     Res.json("Esta es la ruta principal de la pagina");
 }
